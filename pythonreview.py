fc33de2819b7060ff0a8939d5b39057320238a05
malakmi24-meet <139681159+malakmi24-meet@users.noreply.github.com>
2023-07-18 16:31:07 +0300

    I FINISHEDDDDDDDDDDDDDDD

--- pythonreview.py
+++ pythonreview.py
@@ -4,17 +4,21 @@ def create_youtube_video(title, description):
 
 def likes(new_youtube_video):
 	if "likes" in new_youtube_video:
-		new_youtube_video["likes"] = likes + 1;
+		new_youtube_video["likes"] += 1
 	return new_youtube_video
 
 
 def dislikes(new_youtube_video):
 	if "dislikes" in new_youtube_video:
-		new_youtube_video["dislikes"] = dislikes + 1;
+		new_youtube_video["dislikes"] += 1
 	return new_youtube_video
 
 def add_comment(youtubevideo, username, comment_text):
-	youtubevideo["comments"]["username"] = comment_text
+	youtubevideo["comments"][username] = comment_text
 	return new_youtube_video
 
-first_youtube_video = new_youtube_video("title","description")
+first_youtube_video = create_youtube_video("space_dog","with_music")
+first_youtube_video = likes(first_youtube_video)
+first_youtube_video = dislikes(first_youtube_video)
+first_youtube_video = add_comment(first_youtube_video, "malakmiari", "jameel_is_dancing")
+print(first_youtube_video)
